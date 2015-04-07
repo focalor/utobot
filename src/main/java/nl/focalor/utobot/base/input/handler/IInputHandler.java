e17a06c818165d55be4f1ff7de51e5476da856c1
focalor <henkjoost@gmail.com>
2015-04-07 15:18:01 +0200

    prep for help

--- src/main/java/nl/focalor/utobot/base/input/handler/IInputHandler.java
+++ src/main/java/nl/focalor/utobot/base/input/handler/IInputHandler.java
@@ -1,5 +1,7 @@
 package nl.focalor.utobot.base.input.handler;
 
 public interface IInputHandler {
-
+	public default String getHelp() {
+		return null;
+	}
 }
--- src/main/java/nl/focalor/utobot/irc/input/IIrcInputHandler.java
+++ src/main/java/nl/focalor/utobot/irc/input/IIrcInputHandler.java
@@ -2,13 +2,14 @@ package nl.focalor.utobot.irc.input;
 
 import java.util.List;
 import nl.focalor.utobot.base.input.IResult;
+import nl.focalor.utobot.base.input.handler.IInputHandler;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.events.MessageEvent;
 
 /**
  * @author focalor
  */
-public interface IIrcInputHandler {
+public interface IIrcInputHandler extends IInputHandler {
 
 	public List<String> getCommandNames();
 
