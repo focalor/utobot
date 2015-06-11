4e4e77290052ad884ecd75e5aab592fb859b80d1
focalor <focalor@focalor>
2015-06-11 19:41:09 +0200

    bugfix xmpp

--- src/main/java/nl/focalor/utobot/hipchat/input/HipchatXmppListener.java
+++ src/main/java/nl/focalor/utobot/hipchat/input/HipchatXmppListener.java
@@ -1,28 +1,38 @@
 package nl.focalor.utobot.hipchat.input;
 
+import nl.focalor.utobot.base.input.ErrorResult;
+import nl.focalor.utobot.base.input.IResult;
+import nl.focalor.utobot.base.input.MultiReplyResult;
+import nl.focalor.utobot.base.input.NoReplyResult;
+import nl.focalor.utobot.base.input.ReplyResult;
+import nl.focalor.utobot.base.input.listener.AbstractInputListener;
 import nl.focalor.utobot.hipchat.model.HipchatSettings;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import rocks.xmpp.core.Jid;
 import rocks.xmpp.core.XmppException;
 import rocks.xmpp.core.roster.RosterManager;
 import rocks.xmpp.core.roster.model.Contact;
 import rocks.xmpp.core.session.XmppSession;
+import rocks.xmpp.core.stanza.model.AbstractMessage.Type;
+import rocks.xmpp.core.stanza.model.client.Message;
 
 /**
  * @author focalor
  */
 
 @Component
-public class HipchatXmppListener implements Runnable {
+public class HipchatXmppListener extends AbstractInputListener implements Runnable {
 	private final String username;
 	private final String password;
-	private final IHipchatInputListener listener;
 
 	private XmppSession session;
 
 	@Autowired
-	public HipchatXmppListener(HipchatSettings settings, IHipchatInputListener listener) {
-		this.listener = listener;
+	public HipchatXmppListener(HipchatSettings settings) {
 		this.username = settings.getXmppUser();
 		this.password = settings.getXmppPassword();
 
@@ -42,13 +52,32 @@ public class HipchatXmppListener implements Runnable {
 				String msg = event.getMessage().getBody();
 				if (msg != null) {
 					RosterManager rosterManager = session.getManager(RosterManager.class);
-					Contact contact = rosterManager.getContact(event.getMessage().getFrom());
-					listener.onMessage(null, contact.getName(), msg);
+					Jid fromJid = event.getMessage().getFrom();
+					Contact contact = rosterManager.getContact(fromJid);
+
+					IResult result = onMessage(null, contact.getName(), msg);
+
+					if (result == NoReplyResult.NO_REPLY) {
+					} else if (result instanceof ErrorResult) {
+						send(fromJid, ((ErrorResult) result).getMessage());
+					} else if (result instanceof ReplyResult) {
+						send(fromJid, ((ReplyResult) result).getMessage());
+					} else if (result instanceof MultiReplyResult) {
+						MultiReplyResult res = (MultiReplyResult) result;
+						send(fromJid, StringUtils.join(res.getMessages(), "\n"));
+					} else {
+						throw new UnsupportedOperationException("Don't know how to handle result of type "
+								+ result.getClass().getName());
+					}
 				}
 			});
 		} catch (XmppException ex) {
 			throw new RuntimeException("Unexpected exception", ex);
 		}
+	}
+
+	private void send(Jid toJid, String msg) {
+		session.send(new Message(toJid, Type.CHAT, msg));
 	}
 
 	@Override
